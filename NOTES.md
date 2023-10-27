7ad10bec3a8d9194cc01ed66dba8f8a8bd7f845d
João Palmeiro <joaommpalmeiro@gmail.com>
2023-10-27 19:08:33 +0100

    Update script

--- NOTES.md
+++ NOTES.md
@@ -18,6 +18,19 @@
 - https://nodejs.org/download/release/
 - https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V20.md
 - https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V18.md?plain=1
+- https://github.com/rayepps/chiller (for documentation)
+- https://moderndash.io/
+- https://mobily.github.io/ts-belt/
+- https://github.com/rayepps/radash/blob/master/src/typed.ts
+- https://github.com/rayepps/durhuman
+- https://github.com/microsoft/node-jsonc-parser
+  - "In general multiple `EditResults` must not be concatenated because they might impact each other, producing incorrect or malformed JSON data."
+- https://github.com/fabiospampinato/jsonc-simple-parser
+- https://ultimatecourses.com/blog/remove-object-properties-destructuring
+- https://www.samanthaming.com/tidbits/13-skip-values-in-destructuring/
+- https://radash-docs.vercel.app/docs/object/omit
+- https://github.com/fkei/JSON.minify
+- https://github.com/Josee9988/MinifyAllCli
 
 ## Commands
 
@@ -26,7 +39,7 @@ npm init --yes
 ```
 
 ```bash
-npm install -D prettier create-vite jiti json5 fs-extra @types/fs-extra type-fest
+npm install -D prettier create-vite jiti fs-extra @types/fs-extra
 ```
 
 ```bash
@@ -35,4 +48,20 @@ npm install -D @types/node@18.18
 
 ```bash
 fnm ls-remote
+```
+
+```bash
+npm install -D json5 radash type-fest
+```
+
+```bash
+npm install -D jsonc-parser
+```
+
+```bash
+rm -rf node_modules/ && npm install
+```
+
+```bash
+npx minifyall vue/tsconfig.json
 ```
--- build-tsconfigs.ts
+++ build-tsconfigs.ts
@@ -1,4 +1,7 @@
 import { ensureDirSync } from "fs-extra";
+import type { ModificationOptions } from "jsonc-parser";
+import { applyEdits, modify, stripComments } from "jsonc-parser";
+import { readFileSync, writeFileSync } from "node:fs";
 import { resolve } from "node:path";
 
 interface Config {
@@ -6,6 +9,15 @@ interface Config {
   source: string;
   output: string;
 }
+
+function minifyJson(content: string) {
+  return JSON.stringify(JSON.parse(content));
+}
+
+// https://github.com/microsoft/node-jsonc-parser#utilities
+const DEFAULT_MOD_OPTIONS: ModificationOptions = {
+  isArrayInsertion: false,
+};
 
 const VUE_OUTPUT: string = resolve(__dirname, "vue");
 
@@ -23,8 +35,41 @@ const CONFIGS: Config[] = [
 function main() {
   ensureDirSync(VUE_OUTPUT);
 
-  console.log(CONFIGS);
-  // TODO
+  for (const config of CONFIGS) {
+    let configContent = readFileSync(config.source, { encoding: "utf8" });
+    // console.log(configContent);
+
+    configContent = stripComments(configContent);
+    // console.log(configContent)
+
+    // https://www.typescriptlang.org/tsconfig#extends
+    const removeReferences = modify(
+      configContent,
+      ["references"],
+      undefined,
+      DEFAULT_MOD_OPTIONS,
+    );
+    configContent = applyEdits(configContent, removeReferences);
+    // console.log(configContent);
+
+    // https://github.com/vuejs/create-vue/blob/main/template/tsconfig/base/tsconfig.app.json
+    // https://github.com/vuejs/create-vue/blob/main/template/tsconfig/base/tsconfig.node.json
+    // https://github.com/vuejs/create-vue/blob/main/template/tsconfig/base/tsconfig.json
+    const addComposite = modify(
+      configContent,
+      ["compilerOptions", "composite"],
+      true,
+      DEFAULT_MOD_OPTIONS,
+    );
+    configContent = applyEdits(configContent, addComposite);
+    // console.log(configContent);
+
+    // https://github.com/microsoft/node-jsonc-parser/issues/29
+    configContent = minifyJson(configContent);
+    console.log(configContent);
+
+    writeFileSync(resolve(config.output, config.filename), configContent);
+  }
 }
 
 main();
